16da6c96699adeac9db183329323e4c0f4fec51f
yui_dayal <murayamayuui@Yuis-MacBook-Pro.local>
2026-04-22 17:39:35 -0700

    Class specifications and project skeleton

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <cassert>
 #include "Appointment.h"
+#include "Date.h"
+#include "Time.h"
+#include "Location.h"
+#include "Memo.h"
 
 void test_appointment_initialization() {
- 
+    // TODO: Create an Appointment and verify initial state
+    Date date1 = Date("04/22/26");
+    Time time1 = Time("10:00", "17:00");
+    Location location1 = Location("Some restaurant", "Some address");
+    Memo memo1 = Memo("The restaurant has a dress code");
+    Appointment appointment = Appointment(date1, time1, location1, memo1);
+
+    // test if I can get a correct value for date, time, location and memo
+}
+
+int main() {
+    test_appointment_initialization();
 }
